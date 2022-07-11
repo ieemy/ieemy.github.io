7ef818a3a384fff7d6741825f23cc1f69868597c
Ichim Emanuel <ichim.emy24@gmail.com>
2022-07-11 22:03:15 +0300

    add pages

--- index.js
+++ index.js
@@ -19,3 +19,32 @@ console.warn(mottoElement.innerHTML);
 
 //mottoElement.innerHTML = mottoElement.innerHTML + " & " + jobTitle;
 mottoElement.innerHTML += " & " + jobTitle;
+
+function showHomePage() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showSkillsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showProjectsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showLanguagesPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "mome";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "block";
+}
+showHomePage();
