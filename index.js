e8901194a07bb9b2cc72a44b476bd17871683df7
Ichim Emanuel <ichim.emy24@gmail.com>
2022-08-01 21:49:58 +0300

    load skills from json

--- index.js
+++ index.js
@@ -58,13 +58,8 @@ showPage(activePage);
 
 initEvents();
 
-function displaySkills() {
+function displaySkills(skills) {
   var ul = document.querySelector("#skills ul");
-  var skills = [
-    { name: "js", endorcements: 15, favorite: true },
-    { name: "html", endorcements: 5, favorite: true },
-    { name: "css", endorcements: 10, favorite: false },
-  ];
   skills.sort(function (a, b) {
     return b.endorcements - a.endorcements;
     // if (a.name.toLowerCase() < b.name.toLowerCase()) {
@@ -81,4 +76,13 @@ function displaySkills() {
   }
 }
 
-displaySkills();
+function loadSkills() {
+  fetch("skills.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (serverSkills) {
+      displaySkills(serverSkills);
+    });
+}
+loadSkills();
